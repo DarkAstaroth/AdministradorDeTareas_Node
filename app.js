44713920ab3593c3ec7b5d8a1e6b98575b7e881a
Manuel <mninahuanca4@gmail.com>
2021-04-30 10:28:58 -0400

    Implementando stdin y stdout para leer datos

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 require('colors');
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
 console.clear();
 
 const main = () => {
     
-    console.log('Hola Mundo')
+    console.log('Hola Mundo');
+    mostrarMenu();
+    pausa();
 }
 
 main();
