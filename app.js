857cb1962ea7146c615ed20e4f40583c3432b849
Manuel <mninahuanca4@gmail.com>
2021-05-01 17:57:40 -0400

    logica para le manejo de tareas por hacer

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 require('colors');
 const { inquirerMenu,pausa } = require('./helpers/inquirer');
-
-
-console.clear();
+const Tareas = require('./models/tareas');
 
 const main = async () => {
     
