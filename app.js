6291a7620f5e5e864ca3f5a0f3c9cfb13f786d00
Manuel <mninahuanca4@gmail.com>
2021-05-01 01:19:22 -0400

    Agregando opciones al menu interactivo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('colors');
-const { inquirerMenu } = require('./helpers/inquirer');
+const { inquirerMenu,pausa } = require('./helpers/inquirer');
 
 
 console.clear();
@@ -13,6 +13,7 @@ const main = async () => {
 
         opt = await inquirerMenu();
         console.log({ opt });
+        await pausa();
 
     } while (opt !== '0');
     
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -5,9 +5,38 @@ require('colors');
 const preguntas = [
     {
         type: 'list',
-        name: 'opcione',
+        name: 'opcion',
         message: '¿Qué desea hacer?',
-        choices: ['opt1', 'opt2', 'opt3']
+        choices: [
+            {
+                value: '1',
+                name: '1. Crear tarea'
+            },
+            {
+                value: '2',
+                name: '2. Listar tareas'
+            },
+            {
+                value: '3',
+                name: '3. Listar tareas completadas'
+            },
+            {
+                value: '4',
+                name: '4. Listar tareas pendientes'
+            },
+            {
+                value: '5',
+                name: '5. Completar tarea(s)'
+            },
+            {
+                value: '6',
+                name: '6. Borrar tarea'
+            },
+            {
+                value: '0',
+                name: '0. Salir'
+            }
+        ]
     }
 ];
 
@@ -17,7 +46,20 @@ const inquirerMenu = async () => {
     console.log('   Seleccione una opcion'.green);
     console.log('===========================\n'.green);
 
-    const opt = await inquirer.prompt(preguntas);
-    return opt;
+    const { opcion } = await inquirer.prompt(preguntas);
+    return opcion;
 }
-module.exports = {inquirerMenu}
+
+const pausa = async () => {
+    const question = [
+        {
+            type: 'input',
+            name: 'enter',
+            message: `Presione ${'ENTER'.green} para continuar`
+        }
+    ]
+    console.log('\n');  
+    await inquirer.prompt(question);
+
+} 
+module.exports = { inquirerMenu , pausa}
